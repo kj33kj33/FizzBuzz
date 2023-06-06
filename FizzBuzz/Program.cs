0c69df1dfe1a9c0e7441dd01012b02fc10137fff
James <jamesmcdaniel33@hotmail.com>
2023-06-06 17:08:36 -0500

    initial commit

---
+++ FizzBuzz/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+namespace FizzBuzz;
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine(FizBuzz(15));
+    }
+
+    public static string FizBuzz(int number)
+    {
+        var answer = String.Empty;
+
+        if(number % 3 == 0)
+        {
+            answer = "fizz";
+        }
+
+        if(number % 5 == 0)
+        {
+            answer += "buzz";
+        }
+
+        return answer;
+    }
+}
